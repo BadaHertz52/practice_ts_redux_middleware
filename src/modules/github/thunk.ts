18c6474a7271f731d721eb0db12f314f39ec9e4a
BadaHertz52 <badahertz52@gmail.com>
2021-06-12 14:43:02 +0900

    Delete thunk.ts

--- src/modules/github/thunk.ts
+++
@@ -1,5 +0,0 @@
-import { getUserProfile } from "../../api/github";
-import createAsyncThunk from "../../lib/createAsyncThunk";
-import { getUserProfileAsync } from "./actions";
-
-export const getUserProfileThunk = createAsyncThunk(getUserProfileAsync, getUserProfile) ;
